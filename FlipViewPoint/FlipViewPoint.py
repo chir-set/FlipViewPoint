bea33c335f81841e0a1103965ffcea25bef3ccf1
SET <chir.set@free.fr>
2020-07-14 20:13:40 +0200

    Initial commit.

---
+++ FlipViewPoint/FlipViewPoint.py
@@ -0,0 +1,156 @@
+import os
+import unittest
+import logging
+import vtk, qt, ctk, slicer
+from slicer.ScriptedLoadableModule import *
+from slicer.util import VTKObservationMixin
+
+#
+# FlipViewPoint
+#
+
+class FlipViewPoint(ScriptedLoadableModule):
+  """Uses ScriptedLoadableModule base class, available at:
+  https://github.com/Slicer/Slicer/blob/master/Base/Python/slicer/ScriptedLoadableModule.py
+  """
+
+  def __init__(self, parent):
+    ScriptedLoadableModule.__init__(self, parent)
+    self.parent.title = "Flip viewpoint"  # TODO: make this more human readable by adding spaces
+    self.parent.categories = ["Utilities"]  # TODO: set categories (folders where the module shows up in the module selector)
+    self.parent.dependencies = []  # TODO: add here list of module names that this module requires
+    self.parent.contributors = ["SET (Hobbyist)"]  # TODO: replace with "Firstname Lastname (Organization)"
+    # TODO: update with short description of the module and a link to online module documentation
+    self.parent.helpText = """
+Flip 2D views by 180°.
+See more information in <a href="https://github.com/chir-set/PathReformat">module documentation</a>.
+"""
+    # TODO: replace with organization, grant and thanks
+    self.parent.acknowledgementText = """
+This file was originally developed by Jean-Christophe Fillion-Robin, Kitware Inc., Andras Lasso, PerkLab,
+and Steve Pieper, Isomics, Inc. and was partially funded by NIH grant 3P41RR013218-12S1.
+"""
+
+#
+# FlipViewPointWidget
+# These functions can easily be hooked to keyboard shortcuts in
+# slicerrc.py. At one point, we run out of shortcuts,
+# or there are too many to fit in the mind.
+#
+
+class FlipViewPointWidget(ScriptedLoadableModuleWidget, VTKObservationMixin):
+  """Uses ScriptedLoadableModuleWidget base class, available at:
+  https://github.com/Slicer/Slicer/blob/master/Base/Python/slicer/ScriptedLoadableModule.py
+  """
+
+  def __init__(self, parent=None):
+    """
+    Called when the user opens the module the first time and the widget is initialized.
+    """
+    ScriptedLoadableModuleWidget.__init__(self, parent)
+    VTKObservationMixin.__init__(self)  # needed for parameter node observation
+    self.logic = None
+    self._parameterNode = None
+    self._updatingGUIFromParameterNode = False
+
+  def setup(self):
+    """
+    Called when the user opens the module the first time and the widget is initialized.
+    """
+    ScriptedLoadableModuleWidget.setup(self)
+
+    # Load widget from .ui file (created by Qt Designer).
+    # Additional widgets can be instantiated manually and added to self.layout.
+    uiWidget = slicer.util.loadUI(self.resourcePath('UI/FlipViewPoint.ui'))
+    self.layout.addWidget(uiWidget)
+    self.ui = slicer.util.childWidgetVariables(uiWidget)
+
+    # Set scene in MRML widgets. Make sure that in Qt designer the top-level qMRMLWidget's
+    # "mrmlSceneChanged(vtkMRMLScene*)" signal in is connected to each MRML widget's.
+    # "setMRMLScene(vtkMRMLScene*)" slot.
+    uiWidget.setMRMLScene(slicer.mrmlScene)
+
+    # Create logic class. Logic implements all computations that should be possible to run
+    # in batch mode, without a graphical user interface.
+    self.logic = FlipViewPointLogic()
+
+    # Connections
+
+    # Buttons
+    self.ui.redPushButton.connect('clicked(bool)', self.onRedButton)
+    self.ui.greenPushButton.connect('clicked(bool)', self.onGreenButton)
+    self.ui.yellowPushButton.connect('clicked(bool)', self.onYellowButton)
+    self.ui.restorePushButton.connect('clicked(bool)', self.onRestoreButton)
+
+  def onRedButton(self):
+    self.logic.process("Red")
+    
+  def onGreenButton(self):
+    self.logic.process("Green")
+    
+  def onYellowButton(self):
+    self.logic.process("Yellow")
+    
+  def onRestoreButton(self):
+    self.logic.restoreViews()
+
+#
+# FlipViewPointLogic
+#
+
+class FlipViewPointLogic(ScriptedLoadableModuleLogic):
+  """This class should implement all the actual
+  computation done by your module.  The interface
+  should be such that other python code can import
+  this class and make use of the functionality without
+  requiring an instance of the Widget.
+  Uses ScriptedLoadableModuleLogic base class, available at:
+  https://github.com/Slicer/Slicer/blob/master/Base/Python/slicer/ScriptedLoadableModule.py
+  """
+
+  def __init__(self):
+    """
+    Called when the logic class is instantiated. Can be used for initializing member variables.
+    """
+    ScriptedLoadableModuleLogic.__init__(self)
+
+  #https://www.slicer.org/wiki/Documentation/Nightly/ScriptRepository#Change_slice_orientation
+  def process(self, view):
+    sliceNode = slicer.app.layoutManager().sliceWidget(view).mrmlSliceNode()
+    SliceToRAS = sliceNode.GetSliceToRAS()
+    transform=vtk.vtkTransform()
+    transform.SetMatrix(SliceToRAS)
+    transform.RotateY(180)
+    SliceToRAS.DeepCopy(transform.GetMatrix())
+    sliceNode.UpdateMatrices()
+
+  def restoreViews(self):
+    views = ['Red', 'Yellow', 'Green']
+    for view in views:
+        slicer.app.layoutManager().sliceWidget(view).mrmlSliceNode().SetOrientationToDefault()
+
+#
+# FlipViewPointTest
+#
+
+class FlipViewPointTest(ScriptedLoadableModuleTest):
+  """
+  This is the test case for your scripted module.
+  Uses ScriptedLoadableModuleTest base class, available at:
+  https://github.com/Slicer/Slicer/blob/master/Base/Python/slicer/ScriptedLoadableModule.py
+  """
+
+  def setUp(self):
+    """ Do whatever is needed to reset the state - typically a scene clear will be enough.
+    """
+    slicer.mrmlScene.Clear()
+
+  def runTest(self):
+    """Run as few or as many tests as needed here.
+    """
+    self.setUp()
+    self.test_FlipViewPoint1()
+
+  def test_FlipViewPoint1(self):
+    """
+    """
---
+++ README.md
@@ -0,0 +1,17 @@
+# FlipViewPoint module
+
+This [Slicer](https://www.slicer.org/) module flips 2D viewpoints.
+
+**Usage**
+
+Click on a button to flip the respective labeled view.
+
+**Comments**
+
+The view is rotated by 180°.
+
+The Red view is used for reference. Its normal view direction is from bottom to top. We flip it here from top to bottom, and vice-versa.
+
+When a 3D view is displayed from top to bottom by clicking the axis widget on the 'S' label, left and right are inversed with respect to the normal 2D view. Flipping the 2D view to the same orientation of the 3D view may remove much anatomical confusion.
+
+All views may be restored in one click.
